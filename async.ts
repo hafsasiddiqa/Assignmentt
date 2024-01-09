d44ce4a016ece8b3527726c19694fee1ce77ac39
hafsasiddiqa <152436261+hafsasiddiqa@users.noreply.github.com>
2024-01-09 21:53:01 +0500

    Add files via upload

---
+++ async.ts
@@ -0,0 +1,19 @@
+// Question 3: Async/Await: Build a TypeScript function named waitAndGreet...
+
+import chalk from 'chalk';
+
+function wait(ht: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ht));
+}
+
+async function waitAndGreet(name: string): Promise<void> {
+  await wait(3000); 
+  const greeting = `Hello! How have you been? ${name}...`;
+  console.log(chalk.blue(greeting));
+}
+
+async function HT() {
+  await waitAndGreet('Hafsa');
+}
+
+HT();
